a4a1b13545aa827cfd7f76a2ae16349b61b4b071
nhgiang196 <nhgiang196>
2019-05-25 13:36:53 +0700

    commit Jang local

---
+++ src/app/models/user.ts
@@ -0,0 +1,14 @@
+export class User
+{
+    Username: string;
+    Password: string;
+    Token: string;
+    Email?: string;
+    Specification?: string;
+    Deparment? : string;
+    Position? : string;
+    Nickname? : string;
+
+}
+
+
