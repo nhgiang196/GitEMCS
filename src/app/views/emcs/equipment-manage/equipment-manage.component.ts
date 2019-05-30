ef1b17eddd62bca5879f9bd8542e89c04cf722d3
nhgiang196 <nhgiang196>
2019-05-30 15:09:58 +0700

    update equipment (jang get mental heath) :))

--- src/app/views/emcs/equipment-manage/equipment-manage.component.ts
+++ src/app/views/emcs/equipment-manage/equipment-manage.component.ts
@@ -6,12 +6,11 @@ import { NgForm } from '@angular/forms';
 import { Observable, Subject } from 'rxjs';
 import { OperationResult } from 'src/app/helpers/operationResult';
 import { ToastrService } from 'ngx-toastr';
-import $ from 'jquery';
 import { MyHelperService } from 'src/app/services/my-helper.service';
 import { DataTableDirective } from 'angular-datatables';
+import { TranslateService } from '@ngx-translate/core';
 
 const TCode: string = 'EMCS-01' // TCode for Add or Update Equipment
-
 @Component({
   selector: 'app-equipment-manage',
   templateUrl: './equipment-manage.component.html',
@@ -24,94 +23,86 @@ export class EquipmentManageComponent implements OnInit {
   InputManual: ElementRef;
   @ViewChild('myInputFile2')// set for emtpy file after Close or Reload
   InputMehod: ElementRef;
+  constructor(
+    private api: ApiEMCSService,
+    private auth: AuthService,
+    private toastr: ToastrService,
+    public helper: MyHelperService,
+    public trans: TranslateService
+  ) { }
 
+  /****************************************** DECLARATION *******************************************/
   fileUpload = { status: '', message: '', filePath: '' };
   file: File;
-  error: string;
   plansHeader: any[];
-  selectedItem: Department;
+
   manual: Manual;
   equipment: Equipments
   method: Method;
+
   fileName: string;
   operationResult: OperationResult;
   lsEquipments: Equipments[];
-  //Search Parameter
+
+  s_param: {//Search Parameter
+    AssetID: string;
+    EQName: string;
+    Department: string;
+    ProcessDepartment: string;
+    UserName: string;
+    Getall: boolean;
+  }
+
+  actionstatus: string
+
   pAssetID: string;
   pEQName: string;
   pDepartment: string;
   pProcessDepartment: string;
   pUserName: string;
   pGetall: boolean;
-  loading = false;
-  //End Search Parameter
-  _checkAssetID: Boolean;//Use to check AssetID, = false when AssetID already exists.
+  lang: string = this.trans.currentLang.toString();
 
+  //End Search Parameter
+  loading = false;
+  _checkAssetID: Boolean;//Use to check AssetID, = false when AssetID already exists.
   dtTrigger: Subject<any> = new Subject();
 
   lsDepartment: Observable<Department[]>;
-  constructor(
-    private api: ApiEMCSService,
-    private auth: AuthService,
-    private toastr: ToastrService,
-    public helper: MyHelperService
-  ) { }
 
+  /******************************************Init *******************************************/
   ngOnInit() {
     this.auth.nagClass.emcsViewToogle = true;
     this.resetForm();
     this.loadDepartments();
     this.fnSearch();
   }
-
   loadDepartments() {
-    this.api.getDepartment().subscribe((res) => {
-      this.lsDepartment = res as Observable<Department[]>;
-    });
+    this.api.getBasic("Department", this.lang).subscribe((res) => {
+      if (res.length >= 0) {
+        this.lsDepartment = res;
+      }
+      else this.toastr.error("Failed load Department", "Error");
+    })
+
   }
-  /**
-   * Reset All attributes with null
-   * @param form
-   */
-  resetForm(form?: NgForm) {
-    if (form != null)
-      form.resetForm();
+  private resetForm() {
+    // if (form != null) ///????
+    //   form.resetForm();
     this.equipment = {
-      EQID: null,
-      Name: '',
-      AssetID: '',
-      Brand: '',
-      Model: '',
-      UsedDate: null,
-      Stamp: null,
-      UserID: '',
-      IsAdjust: true,
-      State: '',
-      Remark: '',
-      Department: '',
+      EQID: null, Name: '', AssetID: '', Brand: '', Model: '', UsedDate: null, Stamp: null, UserID: '', State: '', Remark: '',
+      Department: this.auth.currentUser.Department,
       ProcessDepartment: '',
+      AdjustType: 'I',
+      Frequency: '3',
       Methods: [],
       Manuals: [],
-      ProcessDeptName: '',
-      DepartmentName: ''
     };
     this.manual = {
-      FileName: '',
-      Name: '',
-      Version: 0,
-      EQID: '',
-      Stamp: null,
-      Remark: '',
-      MethodID: 0
+      FileName: '', Name: '', Version: 0, EQID: '', Stamp: null, Remark: '', MethodID: 0
     };
     this.method = {
-      FileName: '',
-      Name: '',
-      Version: 0,
-      EQID: '',
-      Stamp: null,
-      Remark: '',
-      MethodID: 0
+      FileName: '', Name: '', Version: 0, EQID: '', Stamp: null, Remark: '', MethodID: null
     };
     this.pAssetID = '';
     this.pDepartment = this.auth.currentUser.Department;
@@ -124,28 +115,84 @@ export class EquipmentManageComponent implements OnInit {
     this.InputManual.nativeElement.click();
     this.InputMehod.nativeElement.click();
   }
-
   handleFileInput(files: FileList) {
     this.file = files.item(0);
   }
 
-  refreshGridView() {
+  /******************************************Functions *******************************************/
+  fnSearch() {
+    this.pUserName = (this.pGetall == false ? "" : this.auth.currentUser.Username);
     this.loading = true;
-    this.api.getAllEquipment(this.pAssetID, this.pEQName, this.pDepartment || '', this.pProcessDepartment, this.pUserName).subscribe(res => {
+    /** Refresh grid view */
+    this.api.getAllEquipment(this.pAssetID
+      , this.pEQName
+      , this.pDepartment || ''
+      , this.pProcessDepartment || ''
+      , this.pUserName
+      , this.lang
+    ).subscribe(res => {
       this.lsEquipments = res as Equipments[];
-      this.plansHeader = [];
-      for (var key in this.lsEquipments[0]) {
-        if (key.indexOf('$') < 0) {
-          this.plansHeader.push({ title: key, data: key });
-        }
-      }
       // Calling the DT trigger to manually render the table
-      //this.dtTrigger.next();
       this.loading = false;
-      this.rerender();
+      /** Render */
+      this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
+        // Destroy the table first
+        dtInstance.destroy();
+        // Call the dtTrigger to rerender again
+        this.dtTrigger.next();
+      });
     })
   }
 
+  ngAfterViewInit(): void {
+    this.dtTrigger.next();
+  }
+  ngOnDestroy(): void {
+    // Do not forget to unsubscribe the event
+    this.dtTrigger.unsubscribe();
+  }
+
+  fnEdit(item: Equipments) {
+    this.actionstatus = 'Modify'
+    this._checkAssetID = true;
+    this.api.getDetailEquipment(item.EQID).toPromise().then((res) => {
+      this.equipment = res.Header[0];
+      this.equipment.Manuals = res.Manuals;
+      this.equipment.Methods = res.Methods;
+    })
+  }
+  fnDelete(item: Equipments) {
+    this.api.deleteEquipment(item).subscribe((res) => this.showMessage(res));
+  }
+  //Open Modal
+  fnAdd() {
+    this.actionstatus = 'Add';
+    this.resetForm();
+  }
+
+  //Save Equiment
+  saveEquiment() {
+    if (this.actionstatus == 'Add') {
+      this.equipment.UserID = this.auth.currentUser.Username;
+      this.api.addEquipment(this.equipment).subscribe(res => this.showMessage(res)
+      )
+    } else if (this.actionstatus == 'Modify') {
+      this.api.updateEquipment(this.equipment).subscribe(res => this.showMessage(res)
+      )
+    }
+
+
+  }
+  private showMessage(res: any) {
+    var operationResult = res as OperationResult;
+    if (operationResult.Success) {
+      this.toastr.success(operationResult.Message, operationResult.Caption);
+    } else
+      this.toastr.error(operationResult.Message, operationResult.Caption);
+    this.fnSearch();
+    $("#closeBtn").click();
+  }
+  /******************************************On change event *******************************************/
   onUploadFile(type) {
     const formData = new FormData();
     let newFileName = this.helper.getFileNameWithExtension(this.file);
@@ -153,7 +200,9 @@ export class EquipmentManageComponent implements OnInit {
     formData.append('file', this.file);
     this.api.uploadFile(formData).subscribe(res => {
       this.fileUpload = res;
-    }, err => this.error = err);
+    }, err => {
+      this.toastr.error(err.Message, err.Caption);
+    });
     //1 for Manual
     if (type === 1) {
       this.manual = {
@@ -190,112 +239,25 @@ export class EquipmentManageComponent implements OnInit {
     this.file = null;
 
   }
-  /**
-   * Asset ID is unique
-   * @param AssetID
-   */
+  /*** Asset ID is unique */
   isExistAssetID(AssetID) {
     this._checkAssetID == true;
     this.api.checkAssetID(AssetID).subscribe(res => {
       this._checkAssetID = res > 0 ? false : true;
     })
   }
-
-  onDeleteFile(fileName) {
-    this.api.deleteFile(fileName).subscribe(res => console.log(res))
-  }
-  //Download File without RestAPI
+  /**Download File without RestAPI */
   onGetFile(FileName) {
     let url: string = '/engine-file/';
     url += '/' + FileName;
     window.open(url, '_blank');
   }
 
-  //Internal Form
-
-  fnSearch() {
-    this.pUserName = (this.pGetall == false ? "" : this.auth.currentUser.Username);
-    this.refreshGridView();
-  }
-  rerender(): void {
-    this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
-      // Destroy the table first
-      dtInstance.destroy();
-      // Call the dtTrigger to rerender again
-      this.dtTrigger.next();
-    });
-  }
-  ngAfterViewInit(): void {
-    this.dtTrigger.next();
-  }
-
-  ngOnDestroy(): void {
-    // Do not forget to unsubscribe the event
-    this.dtTrigger.unsubscribe();
-  }
-
-
-  fnEdit(item: Equipments) {
-    this._checkAssetID = true;
-    this.api.getDepartment().subscribe((res) => {
-      this.lsDepartment = res as Observable<Department[]>;
-    });
-
-    this.api.getDetailEquipment(item.EQID).toPromise().then((res) => {
-      this.equipment = res.Header[0];
-      this.equipment.Manuals = res.Manuals;
-      this.equipment.Methods = res.Methods;
-    })
-  }
-  fnDelete(item: Equipments) {
-    this.api.deleteEquipment(item).subscribe((res) => this.messageRespone(res));
-  }
-  //Open Modal
-  fnAdd() {
-    this.resetForm();
-  }
-
-  fnClose() {
-    // this.equipment.Manuals.forEach(element => {
-    //   this.onDeleteFile(element.FileName);
-    // });
-    // this.equipment.Methods.forEach(element => {
-    //   this.onDeleteFile(element.FileName);
-    // });
-    this.resetForm();
-  }
-
-  //Save Equiment
-  saveEquiment() {
-
-    if (this.equipment.EQID == null) {
-      this.equipment.UserID = this.auth.currentUser.Username;
-      this.api.addEquipment(this.equipment).subscribe(res => {
-        this.messageRespone(res);
-        $("#closeBtn").click();
-      })
-    } else {
-      this.api.updateEquipment(this.equipment).subscribe(res => this.messageRespone(res))
-    }
-
-  }
-  //Respone Message after execute
-  messageRespone(res: any) {
-    this.operationResult = res as OperationResult;
-    if (this.operationResult.Success) {
-      this.toastr.success(this.operationResult.Message, this.operationResult.Caption);
-      this.resetForm();
-      this.fnSearch();
-      $('#btnClose').click();
-    } else
-      this.toastr.error(this.operationResult.Message, this.operationResult.Caption);
-  }
-
-  //Delete Manual Item
+  /**Delete Manual Item */
   onDeleteManualFile(item) {
     this.equipment.Manuals.splice(this.equipment.Manuals.indexOf(item), 1);
   }
-  //Delete Method Item
+  /**Delete Method Item */
   onDeleteMethodFile(item) {
     this.equipment.Methods.splice(this.equipment.Methods.indexOf(item), 1);
   }
